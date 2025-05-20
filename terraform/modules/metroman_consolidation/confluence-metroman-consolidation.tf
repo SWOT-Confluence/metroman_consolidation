3af912eb13776c4f10648a750c3aaf042a2a2294
Josh Garde <joshgarde@gmail.com>
2025-05-20 05:48:20 -0700

    Parameterize Terraform Child Module (#5)
    * Initial parameterization refactor
    
    * default_tags padding
    
    * Fix references to EFS and IAM roles in module
    
    ---------
    
    Co-authored-by: Nikki <17799906+nikki-t@users.noreply.github.com>

--- terraform/modules/metroman_consolidation/confluence-metroman-consolidation.tf
+++ terraform/modules/metroman_consolidation/confluence-metroman-consolidation.tf
@@ -1,54 +1,57 @@
 # Job Definition
 resource "aws_batch_job_definition" "generate_batch_jd_metroman_consolidation" {
-  name                  = "${var.prefix}-metroman-consolidation"
-  type                  = "container"
-  container_properties  = <<CONTAINER_PROPERTIES
-  {
-    "image": "${local.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.prefix}-metroman-consolidation",
-    "executionRoleArn": "${data.aws_iam_role.exe_role.arn}",
-    "jobRoleArn": "${data.aws_iam_role.job_role.arn}",
-    "fargatePlatformConfiguration": { "platformVersion": "LATEST" },
-    "logConfiguration": {
-      "logDriver" : "awslogs",
-      "options": {
-        "awslogs-group" : "${data.aws_cloudwatch_log_group.cw_log_group.name}"
-      }
-    },
-    "resourceRequirements": [
-      {"type": "MEMORY", "value": "16384"},
-      {"type": "VCPU", "value": "2"}
-    ],
-    "mountPoints": [
-      {
-        "sourceVolume": "input",
-        "containerPath": "/mnt/data/input",
-        "readOnly": true
-      },
-      {
-        "sourceVolume": "flpe",
-        "containerPath": "/mnt/data/flpe",
-        "readOnly": false
-      }
-    ],
-    "volumes": [
-      {
-        "name": "input",
-        "efsVolumeConfiguration": {
-          "fileSystemId": "${data.aws_efs_file_system.aws_efs_input.file_system_id}",
-          "rootDirectory": "/"
-        }
-      },
-      {
-        "name": "flpe",
-        "efsVolumeConfiguration": {
-          "fileSystemId": "${data.aws_efs_file_system.aws_efs_flpe.file_system_id}",
-          "rootDirectory": "/metroman"
-        }
-      }
-    ]
-  }
-  CONTAINER_PROPERTIES
+  name = "${var.prefix}-metroman-consolidation"
+  type = "container"
   platform_capabilities = ["FARGATE"]
-  propagate_tags        = true
-  tags = { "job_definition": "${var.prefix}-metroman-consolidation" }
+  propagate_tags = true
+  tags = { job_definition = "${var.prefix}-metroman-consolidation" }
+
+  container_properties  = jsonencode({
+    image = "${local.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.prefix}-metroman-consolidation:${var.image_tag}"
+    executionRoleArn = var.iam_execution_role_arn
+    jobRoleArn = var.iam_job_role_arn
+    fargatePlatformConfiguration = {
+      platformVersion = "LATEST"
+    }
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group = aws_cloudwatch_log_group.cw_log_group.name
+      }
+    }
+    resourceRequirements = [{
+      type = "MEMORY"
+      value = "16384"
+    }, {
+      type = "VCPU",
+      value = "2"
+    }]
+    mountPoints = [{
+      sourceVolume = "input",
+      containerPath = "/mnt/data/input"
+      readOnly = false
+    }, {
+      sourceVolume = "flpe"
+      containerPath = "/mnt/data/flpe"
+      readOnly = false
+    }]
+    volumes = [{
+      name = "input"
+      efsVolumeConfiguration = {
+        fileSystemId = var.efs_file_system_ids["input"]
+        rootDirectory = "/"
+      }
+    }, {
+      name = "flpe"
+      efsVolumeConfiguration = {
+        fileSystemId = var.efs_file_system_ids["flpe"]
+        rootDirectory = "/metroman"
+      }
+    }]
+  })
+}
+
+# Log group
+resource "aws_cloudwatch_log_group" "cw_log_group" {
+  name = "/aws/batch/job/${var.prefix}-metroman-consolidation/"
 }
